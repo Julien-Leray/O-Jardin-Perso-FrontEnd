638d16aa5ba65320b0e98fb89bce25e43343d9c2
Flavie-A <flavie.andre.pro@gmail.com>
2024-05-14 23:02:40 +0200

    feat:mobile-first breakpoint dans app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,15 @@
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
 
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-full md:w-3/4 md:mx-auto">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline text-pink-500">
           Hello world!
         </h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="" alt="logo" />
 
         <p>
           Edit <code>src/components/App/App.tsx</code> and save to reload.
