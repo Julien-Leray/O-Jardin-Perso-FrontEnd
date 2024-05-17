18e5bf1cbea4f4dfad25fec942ee4ce49e2b6955
Your Name <you@example.com>
2024-05-17 14:05:45 +0200

    Revert "Merge pull request #23 from O-clock-Muffin/features/path"
    This reverts commit 53bf98eee32cefb1c8fcfa0a5dde92a606fdb5bb, reversing
    changes made to 5be51990fe4a82432b633b8795df668089e06689.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './components/App/App';
+
 import './styles/index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
