1947e6789d1100a5c89d23d3122a85089835fd65
SURY Kim <kim.sury@oclock.school>
2024-05-17 09:39:53 +0200

    feature:tuto-on-home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
+
 
 function Home() {
+
+  const [tuto, setTuto] = useState([]);
   return (
     <div>
       <h1>Page d'Accueil</h1>
