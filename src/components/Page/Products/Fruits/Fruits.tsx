ed825d164d5adee67e5defdb644f987dd471f83e
Your Name <you@example.com>
2024-05-22 14:09:29 +0200

    Setting up TailWind for the fruits and vegetables page

--- src/components/Page/Products/Fruits/Fruits.tsx
+++ src/components/Page/Products/Fruits/Fruits.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
 import { fetchFruits } from '../../../../store/thunks/productThunks';
 
 function Fruits() {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const { fruits, loading, error } = useAppSelector((state) => state.products);
 
@@ -13,23 +12,38 @@ function Fruits() {
   }, [dispatch]);
 
   return (
-    <div className="container mx-auto mt-5">
-      <h1 className="text-2xl font-bold mb-3">Fruits</h1>
-      {loading && <p>Chargement...</p>}
-      {error && <p>Erreur : {error}</p>}
-      <ul>
+    <div className="container mx-auto mt-5 p-5 mb-5 bg-[#F6D50E]">
+      <h1 className="text-2xl font-bold text-white text-center mb-5">Fruits</h1>
+      {loading && <p className="text-center">Chargement...</p>}
+      {error && <p className="text-red-600 text-center">Erreur : {error}</p>}
+      <div className="grid grid-cols-3 gap-4">
         {fruits.map((fruit) => (
-          <li key={fruit.id} className="border-b border-gray-200 py-2">
-            <button
-              type="button"
-              onClick={() => navigate(`/fruits/${fruit.id}`)}
-              className="text-blue-500 cursor-pointer hover:underline"
-            >
-              {fruit.name}
-            </button>
-          </li>
+          <div
+            key={fruit.id}
+            className="max-w-sm rounded overflow-hidden shadow-lg bg-[#16A1AF]"
+          >
+            <img
+              className="w-full h-48 object-cover container mx-auto mt-5 mb-5 p-5"
+              src={`http://localhost:4000${fruit.picture}`}
+              alt={fruit.name}
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-white text-center mb-2">{fruit.name}</div>
+              <p className="text-gray-700 text-base">
+                {fruit.description.slice(0, 100)}...
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <Link
+                to={`/fruits/${fruit.id}`}
+                className="mt-3 mb-2 py-2 px-4 rounded flex justify-center w-full text-sm font-medium text-white bg-[#F5780A] rounded-full hover:bg-black focus:ring-1 focus:ring-[F6D50E]"
+              >
+                Lire la suite
+              </Link>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
--- src/components/Page/Products/Legumes/Legumes.tsx
+++ src/components/Page/Products/Legumes/Legumes.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Ajout de Link ici
 import { useAppDispatch, useAppSelector } from '../../../../hooks/redux';
 import { fetchLegumes } from '../../../../store/thunks/productThunks';
 
 function Legumes() {
-  const navigate = useNavigate();
   const dispatch = useAppDispatch();
   const { legumes, loading, error } = useAppSelector((state) => state.products);
 
@@ -13,23 +12,40 @@ function Legumes() {
   }, [dispatch]);
 
   return (
-    <div className="container mx-auto mt-5">
-      <h1 className="text-2xl font-bold">Légumes</h1>
-      {loading && <p>Chargement...</p>}
-      {error && <p>Erreur : {error}</p>}
-      <ul>
+    <div className="container mx-auto mt-5 p-5 mb-5 bg-[#F6D50E]">
+      <h1 className="text-2xl font-bold text-white text-center mb-5">
+        Légumes
+      </h1>
+      {loading && <p className="text-center">Chargement...</p>}
+      {error && <p className="text-red-600 text-center">Erreur : {error}</p>}
+      <div className="grid grid-cols-3 gap-4">
         {legumes.map((legume) => (
-          <li key={legume.id} className="border-b border-gray-200 py-2">
-            <button
-              type="button"
-              onClick={() => navigate(`/legumes/${legume.id}`)}
-              className="text-blue-500 cursor-pointer hover:underline"
-            >
-              {legume.name}
-            </button>
-          </li>
+          <div
+            key={legume.id}
+            className="max-w-sm rounded overflow-hidden shadow-lg bg-[#16A1AF]"
+          >
+            <img
+              className="w-full h-48 object-cover container mx-auto mt-5 mb-5 p-5"
+              src={`http://localhost:4000${legume.picture}`} // Assurez-vous que le chemin d'accès est correct
+              alt={legume.name}
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{legume.name}</div>
+              <p className="text-gray-700 text-base">
+                {legume.description.slice(0, 100)}...
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <Link
+                to={`/legumes/${legume.id}`}
+                className="mt-3 mb-2 py-2 px-4 rounded flex justify-center w-full text-sm font-medium text-white bg-[#F5780A] rounded-full hover:bg-black focus:ring-1 focus:ring-[F6D50E]"
+              >
+                Lire la suite
+              </Link>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
