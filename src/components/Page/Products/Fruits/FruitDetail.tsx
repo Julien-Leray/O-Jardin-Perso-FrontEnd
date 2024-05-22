c4710e4ee90971c20c28d7780ce2b80bf04823fe
Your Name <you@example.com>
2024-05-22 12:18:31 +0200

    Fix: solving the problem of displaying the details of fruits and vegetables and connecting the components to the Back locally

--- src/components/Page/Products/Fruits/FruitDetail.tsx
+++ src/components/Page/Products/Fruits/FruitDetail.tsx
@@ -4,20 +4,22 @@ import { useAppSelector, useAppDispatch } from '../../../../hooks/redux';
 import { fetchFruitDetails } from '../../../../store/thunks/productThunks';
 
 function FruitDetail() {
-  const { id } = useParams();
+  // Renommez nom_fruit en nomFruit
+  const { nomFruit } = useParams();
   const dispatch = useAppDispatch();
   const { selectedFruit, loading, error } = useAppSelector(
     (state) => state.products
   );
 
   useEffect(() => {
-    if (id) {
-      dispatch(fetchFruitDetails(id));
+    // Utilisez nomFruit renommé pour le dispatch
+    if (nomFruit) {
+      dispatch(fetchFruitDetails(nomFruit));
     }
-  }, [id, dispatch]);
+  }, [nomFruit, dispatch]);
 
   if (loading) return <div>Chargement...</div>;
-  if (error) return <div>Erreur: {error}</div>;
+  if (error) return <div>Erreur : {error}</div>;
   if (!selectedFruit) return <div>Aucun fruit sélectionné.</div>;
 
   return (
