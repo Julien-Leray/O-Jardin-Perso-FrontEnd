b78d35c806dc400eaee2e1955481d0751976e652
Flavie-A <flavie.andre.pro@gmail.com>
2024-05-22 12:41:54 +0200

    fix:fusion pb after merge

--- src/components/Page/Home/Home.tsx
+++ src/components/Page/Home/Home.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import tutoData from '../../../data/tuto';
 import { useAppSelector, useAppDispatch } from '../../../hooks/redux';
-import { fetchTutorials} from '../../../store/reducers/tutoriels';
-import { fetchFruits, fetchLegumes } from '../../../store/reducers/products';
+import { fetchTutorials } from '../../../store/reducers/tutoriels';
+import products from '../../../store/reducers/products';
+import { fetchFruits, fetchLegumes } from '../../../store/thunks/productThunks';
 
 interface Tutorials {
   id: number;
@@ -20,36 +20,35 @@ interface Product {
 
 const getRandomTutos = (tuto: Tutorials[]): Tutorials[] => {
   if (tuto.length <= 2) return tuto;
-  const twoTutoOnHome= [...tuto].sort(() => 0.5 - Math.random());
-  return twoTutoOnHome.slice(0, 2);
+  const twoTutoOnHome = [...tuto].sort(() => 0.5 - Math.random());
+  return twoTutoOnHome.slice(0, 3);
 };
 
 const getRandomFruits = (fruits: Product[]): Product[] => {
   if (fruits.length <= 2) return fruits;
-  const fruitsOnHome= [...fruits].sort(() => 0.5 - Math.random());
-  return fruitsOnHome.slice(0, 2);
+  const fruitsOnHome = [...fruits].sort(() => 0.5 - Math.random());
+  return fruitsOnHome.slice(0, 4);
 };
 
 const getRandomLegumes = (legumes: Product[]): Product[] => {
   if (legumes.length <= 2) return legumes;
-  const legumeOnHome= [...legumes].sort(() => 0.5 - Math.random());
-  return legumeOnHome.slice(0, 2);
+  const legumeOnHome = [...legumes].sort(() => 0.5 - Math.random());
+  return legumeOnHome.slice(0, 4);
 };
 
 function Home() {
-
   const dispatch = useAppDispatch();
-  const {tuto} = useAppSelector((state) => state.tutoriels);
+  const { tuto } = useAppSelector((state) => state.tutoriels);
 
   const randomTutos = getRandomTutos(tuto);
 
-  const {legumes} = useAppSelector((state) => state.products);
+  const { legumes } = useAppSelector((state) => state.products);
 
   const randomLegumes = getRandomLegumes(legumes);
 
   const { fruits } = useAppSelector((state) => state.products);
 
-  const randomFruits= getRandomFruits(fruits);
+  const randomFruits = getRandomFruits(fruits);
 
   useEffect(() => {
     dispatch(fetchTutorials());
@@ -57,19 +56,18 @@ function Home() {
     dispatch(fetchFruits());
   }, [dispatch]);
 
-  
   return (
     <>
       <div className="bg-white text-center py-2 text-sm italic">
         <p>Bienvenue sur notre site web dédié au jardinage!</p>
       </div>
-
       <div className="flex flex-col md:flex-row gap-2 md:gap-6">
         <div className="w-full py-4">
           <h2 className="text-xl text-center font-bold	p-2">Fruits </h2>
           <div className="flex flex-col bg-gray-200 rounded-lg p-4">
             <ul className="flex flex-wrap justify-around  ">
-            {randomFruits.map((product)=>(                <li className="my-0.5 p-1 w-5/12 " key={product.id}>
+              {randomFruits.map((product) => (
+                <li className="my-0.5 p-1 w-5/12 " key={product.id}>
                   <img
                     // src={product.picture}
                     alt={`Photo de ${product.name}`}
@@ -91,7 +89,8 @@ function Home() {
           <h2 className="text-xl text-center font-bold p-2">Légumes </h2>
           <div className="flex flex-col bg-gray-200 rounded-lg p-4">
             <ul className="flex flex-wrap justify-around  ">
-            {randomLegumes.map((product)=>(                <li className="my-0.5 p-1 w-5/12 " key={product.id}>
+              {randomLegumes.map((product) => (
+                <li className="my-0.5 p-1 w-5/12 " key={product.id}>
                   <img
                     // src={product.picture}
                     alt={`Photo de ${product.name}`}
@@ -110,12 +109,12 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-[#16A1AF] py-1 my-2 mx-4 rounded ">
-        <h2 className="text-black text-base">Tuto Jardinage</h2>
-        <ul className="text-xs rounded py-3 md:flex md:justify-around m-15 flex flex-col items-center">
+      <div className=" flex flex-col rounded  py-6">
+        <h2 className="text-xl text-center font-bold p-4">Tutos jardinage</h2>
+        <ul className="m-15 flex flex-crow items-center gap-2 md:gap-6">
           {randomTutos.map((tutoriels) => (
             <li
-              className="my-0.5 mx-0.5 p-1 text-black m-1.5 w-full"
+              className="flex flex-col bg-gray-200 p-4 w-full rounded-lg"
               key={tutoriels.id}
             >
               <img
@@ -123,16 +122,19 @@ function Home() {
                 src={tutoriels.picture}
                 alt={`Tuto de ${tutoriels.title}`}
               />
-              <h3 className=" bg-white p-1">{tuto.title}</h3>
+              <h3 className="font-bold">{tutoriels.title}</h3>
               <p className="p-1">{tutoriels.article}</p>
-              <p className="bg-[#F5780A] rounded-full px-2 w-36 text-sm md:text-base m-auto my-1 text-white">
-                Lire la suite
-              </p>
+              <Link
+                to="/tutos"
+                className="mx-auto py-4 px-6 text-white bg-[#F5780A] rounded-full hover:bg-black focus:ring-1 focus:ring-[#F6D50E]"
+              >
+                <button type="button">Lire la suite</button>
+              </Link>
             </li>
           ))}
         </ul>
       </div>
-    </div>
+    </>
   );
 }
 
