e4d8aa1162ac9241ec9169ef0b216ac04f17b616
Flavie-A <flavie.andre.pro@gmail.com>
2024-06-04 16:02:15 +0200

    feat:modif env files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-export default defineConfig({
-  plugins: [react()],
-  sourcemap: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_API_URL), // Access the variable with the VITE_ prefix
+    },
+    // Other Vite configuration options if needed
+  };
 });
