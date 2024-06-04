0984f98b9ba2cc32dabebfc2936a7b04cd6e8316
Flavie-A <flavie.andre.pro@gmail.com>
2024-06-04 16:10:13 +0200

    feat:commit after only dist

--- README.md
+++
@@ -1,1 +0,0 @@
-# projet-compagnon-de-jardin-front
--- src/store/thunks/productThunks.ts
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axiosInstance from '../../axios/axios';
-
-const fetchAllProducts = createAsyncThunk('products/fetchAll', async () => {
-  const response = await axiosInstance.get('/products/');
-  return response.data;
-});
-export default fetchAllProducts;
