c5b00be38c9014f40ab4d3bea2e3c1e5b2e87017
Quentin-Lec <quentin.lecour@gmail.com>
2024-07-01 17:34:30 +0200

    fix : test

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}${import.meta.env.VITE_API_DEPLOY || '/api'}`,
+  baseURL: `${import.meta.env.VITE_API_URL}`,
 });
 
 export function addTokenToAxiosInstance(token: string) {
